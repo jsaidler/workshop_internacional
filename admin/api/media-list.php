8f95af10f8e23c3a1cb959e071629bca43443c0c
jsaidler <jsaidler@gmail.com>
2026-09-12 18:26:44 -0300

    Include tags in media library listing

--- admin/api/media-list.php
+++ admin/api/media-list.php
@@ -1,2 +1,3 @@
 <?php
-require __DIR__.'/../../app/bootstrap.php';require_admin();header('Content-Type: application/json; charset=UTF-8');echo json_encode(['items'=>media_list(database()),'capabilities'=>media_capabilities(),'csrf'=>csrf_token('media')],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+declare(strict_types=1);
+require __DIR__.'/../../app/bootstrap.php';require_admin();header('Content-Type: application/json; charset=UTF-8');$db=database();$items=media_list($db);foreach($items as &$item)$item['tags']=media_asset_tags($db,(int)$item['id']);unset($item);echo json_encode(['items'=>$items,'capabilities'=>media_capabilities(),'csrf'=>csrf_token('media')],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
