b3daa9abdac603e2e48a01a31eda0b8d5247edca
jsaidler <jsaidler@gmail.com>
2026-09-12 17:41:27 -0300

    Hotfix: restore proven public renderer while CMS editor is repaired

--- index.php
+++ index.php
@@ -1,16 +1,2 @@
 <?php
-declare(strict_types=1);
-require __DIR__.'/app/bootstrap.php';
-security_headers();
-try{
-    $db=database();
-    $activity=public_activity();
-    $locale=public_locale();
-    $page=cms_current_page($db,$activity,$locale);
-    if(!$page||empty($page['published_document_json']))throw new RuntimeException('page_not_found');
-    cms_render_public_page($activity,$page,cms_page_doc($page,true),false);
-}catch(RuntimeException $error){
-    http_response_code(404);
-    header('Content-Type: text/plain; charset=UTF-8');
-    echo 'Page not found';
-}
+require __DIR__.'/app/bootstrap.php'; security_headers(); require __DIR__.'/template/public.php'; try{render_public_page([],[],isset($_GET['success']));}catch(RuntimeException $e){http_response_code(404);echo 'Activity not found';}
